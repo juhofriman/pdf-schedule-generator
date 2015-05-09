08bac18ede532f144219bb7faede59a2afeaf4f3
Juho Friman <juho.friman@uta.fi>
2015-05-09 10:53:24 +0300

    Documented clocktime functions.

--- src/otm_schedule_creator/clocktime.clj
+++ src/otm_schedule_creator/clocktime.clj
@@ -1,7 +1,9 @@
 (ns otm-schedule-creator.clocktime
   (:require [clojure.string :as string]))
 
-(defn ^:private
+(defn
+  ^{:private true
+    :doc "Throws if hours or minutes are illeagal" }
   fail-if-illegal
   [[hours mins :as valuevec]]
     (cond
@@ -11,7 +13,9 @@
         (throw (IllegalArgumentException. (str "hours " hours " is illegal")))
       :else valuevec))
 
-(defn ^:private
+(defn
+  ^{:private true
+    :doc "Removes leading 0 from string" }
   remove-preceeding-zero
   [s]
   (let [a (seq s)]
@@ -20,11 +24,8 @@
       (string/join a))))
 
 (defn
-  str-to-clocktime
-  [s]
-    (fail-if-illegal (vec (map #(read-string (remove-preceeding-zero %)) (string/split s #":")))))
-
-(defn ^:private
+  ^{:private true
+    :doc "Transforms int to string and adds leading 0 if needed" }
   pad-zero-if-needed
   [n]
   (if
@@ -33,12 +34,20 @@
       (str n)))
 
 (defn
+  str-to-clocktime
+  "Transforms clocktime string ('12:00') representation to vector [12 0]"
+  [s]
+    (fail-if-illegal (vec (map #(read-string (remove-preceeding-zero %)) (string/split s #":")))))
+
+(defn
   clocktime-to-str
+  "Transforms clocktime vector [12 0] to string 12:00"
   [[hours minutes]]
   (str (pad-zero-if-needed hours) ":" (pad-zero-if-needed minutes)))
 
 (defn
   clocktime-add
+  "Adds minutes and/or hours to clocktime vector"
   [[h m] & {:keys [minutes hours]
             :or { minutes 0 hours 0}}]
     (let [rawh (+ h hours)  rawm(+ m minutes)]
